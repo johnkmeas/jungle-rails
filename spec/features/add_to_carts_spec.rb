8e486bd53a0b4dbd512313cc1fb3cf5ec28e2cb3
johnkmeas <johnkmeas@gmail.com>
2017-02-22 00:50:46 +0000

    finished implementing testing of add to cart functionality

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Clicking add to cart will increase the contents of my Cart from empty to 1" do
+    # ACT
+    visit root_path
+
+    first('article.product').find_link('Add').click
+
+    sleep(5)
+    save_screenshot
+
+    expect(page).to have_text "My Cart (1)"
+  end
+end
